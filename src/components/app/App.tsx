75763aa10e203d18a5d6fdd24f8473012cb659e5
Alex White <shshchkn@yandex.ru>
2022-10-01 14:35:09 +0200

    App upd.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from '../../logo.svg';
 import app from './app.module.scss';
 
+import AppHeader from '../header/Header';
+
 function App() {
   return (
-    <div className={app.app}>
-      <header className={app.header}>
-        <img src={logo} className={app.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={app.link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={app.wrapper}>
+      <AppHeader />
+      <main className={app.main}>
+        Main content
+      </main>
     </div>
-  );
+  // <div className={app.app}>
+  //   <header className={app.header}>
+  //     <img src={logo} className={app.logo} alt="logo"/>
+  //     <p>
+  //       Edit <code>src/App.tsx</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className={app.link}
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     > Learn React </a>
+  //   </header>
+  // </div>
+)
+  ;
 }
 
 export default App;
