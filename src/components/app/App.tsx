61cabe19fea9b66b9738f38e43550797d0ae1934
Alex White <shshchkn@yandex.ru>
2022-10-09 10:20:02 +0200

    App upd.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,23 +1,80 @@
-import React from 'react';
-
-import app from './app.module.scss';
+import React, {useEffect, useState} from 'react';
+import {InfoIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 
 import AppHeader from '../app-header/AppHeader';
 import BurgerIngredients from '../burger-ingredients/BurgerIngredients';
 import BurgerConstructor from '../burger-constructor/BurgerConstructor';
 
+import styles from './app.module.scss';
+
+const dataUrl = 'https://norma.nomoreparties.space/api/ingredients';
+
+class ErrorBoundary extends React.Component {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+
+  static getDerivedStateFromError(error: boolean) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: any, info: any) {
+    console.error("Возникла ошибка!", error, info);
+  }
+
+  render() {
+    // @ts-ignore
+    if (this.state.hasError) {
+      return (
+        <section className={styles.error}>
+          <span className={`${styles.error__icon} mb-4`}>
+            <InfoIcon type="primary" />
+          </span>
+          <h1 className={`${styles.error__title} text text_type_main-medium mb-4`}>Что-то пошло не так :(</h1>
+          <p className={`${styles.error__text} text text_type_main-default text_color_inactive`}>
+            В приложении произошла ошибка. <br/>Пожалуйста, перезагрузите страницу.
+          </p>
+        </section>
+      );
+    }
+    // @ts-ignore
+    return this.props.children;
+  }
+}
+
+
 const App = () => {
+  const [state, setState] = useState({
+    data: null,
+    loading: true
+  })
+
+  useEffect(() => {
+    setState({...state, loading: true});
+    fetch(dataUrl)
+      .then(res => res.json())
+      .then(res => setState({...state, data: res.data, loading: false}))
+      .catch(err => console.log(err));
+  }, []);
+
+  const {data, loading} = state;
+
   return (
-    <div className={app.wrapper}>
-      <AppHeader/>
-      <main className={app.main}>
-        <div className="container">
-          <div className={app.dashboard}>
-            <BurgerIngredients title="Соберите бургер"/>
-            <BurgerConstructor />
+    <div className={styles.wrapper}>
+      <ErrorBoundary>
+        <AppHeader/>
+        <main className={styles.main}>
+          <div className="container">
+            <div className={styles.dashboard}>
+              <BurgerIngredients products={data} title="Соберите бургер"/>
+              <BurgerConstructor products={data} />
+            </div>
           </div>
-        </div>
-      </main>
+        </main>
+      </ErrorBoundary>
     </div>
   );
 }
