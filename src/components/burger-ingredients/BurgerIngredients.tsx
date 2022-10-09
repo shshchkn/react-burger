df0d20c003673dfbaebbaad007cb63b332c4618c
Alex White <shshchkn@yandex.ru>
2022-10-09 10:20:19 +0200

    Ingredients upd.

--- src/components/burger-ingredients/BurgerIngredients.tsx
+++ src/components/burger-ingredients/BurgerIngredients.tsx
@@ -1,11 +1,11 @@
 import React, {useState, useEffect, useMemo} from 'react';
 import {Tab, Counter, CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
+import ProductsList from '../products-list/ProductsList';
+
 import Modal from "../modal/Modal";
 
 import burgerIngredients from './burger-ingredients.module.scss';
-
-import data from '../../utils/data';
 
 const TabsBlock = () => {
   const [current, setCurrent] = useState('bun');
@@ -25,58 +25,13 @@ const TabsBlock = () => {
   );
 }
 
-type IngredientProps = {
-  _id: string,
-  name: string,
-  image: string,
-  price: number,
-  count: number,
-  showModal?: () => void
-}
-
-const Ingredient = ({_id, name, image, price, count, showModal}: IngredientProps) => {
-  return (
-    <li
-      className={`${burgerIngredients.ingredients__list_item} ${burgerIngredients.card}`}
-      onClick={showModal}
-      title={name}>
-      {count && <Counter count={count} size="default"/>}
-      <div className={`${burgerIngredients.card__image} mb-2 ml-4 mr-4`}>
-        <img src={image} alt={name}/>
-      </div>
-      <div className={`${burgerIngredients.card__price} mb-2`}>
-        <span className="text text_type_digits-default mr-2">{price}</span>
-        <CurrencyIcon type="primary" />
-      </div>
-      <p className={`${burgerIngredients.card__title} text text_type_main-default`}>{name}</p>
-    </li>
-  );
-}
-
-type IngredientsProps = {
-  title: string,
-  type: string,
-  showModal?: () => void
-}
-
-const Ingredients = ({title, type, showModal}: IngredientsProps) => {
-  return (
-    <div className="ingredients__section_block ingredients__block mb-10" id={type}>
-      <h2 className="ingredients__block_title text text_type_main-medium mb-6">{title}</h2>
-      <ul className={`ingredients__block_list ${burgerIngredients.ingredients__list}`} role="list">
-        {data.map((item) =>
-          (item.type === type && <Ingredient showModal={showModal} key={item._id} count={1} {...item}/>)
-        )}
-      </ul>
-    </div>
-  );
-}
 
 type BurgerIngredientsProps = {
   title: string,
+  products?: Array<any> | null
 }
 
-const BurgerIngredients = ({title}: BurgerIngredientsProps) => {
+const BurgerIngredients = ({title, products}: BurgerIngredientsProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleOpenModal = () => {
@@ -92,11 +47,23 @@ const BurgerIngredients = ({title}: BurgerIngredientsProps) => {
       <h1 className={`${burgerIngredients.ingredients__title} mb-5`}>{title}</h1>
       <TabsBlock />
       <div className={`${burgerIngredients.ingredients__section} custom-scroll mt-10`}>
-        <Ingredients showModal={handleOpenModal} title="Булки" type="bun"/>
-        <Ingredients showModal={handleOpenModal} title="Соусы" type="sauce"/>
-        <Ingredients showModal={handleOpenModal} title="Начинки" type="main"/>
+        <ProductsList data={products} showModal={handleOpenModal} title="Булки" type="bun"/>
+        <ProductsList data={products} showModal={handleOpenModal} title="Соусы" type="sauce"/>
+        <ProductsList data={products} showModal={handleOpenModal} title="Начинки" type="main"/>
       </div>
-      <Modal show={isOpen} onClose={handleCloseModal}> MODAL </Modal>
+      <Modal headerTitle="Детали ингредиента" show={isOpen} onClose={handleCloseModal}>
+        <div className="card card_modal">
+          <div className="card__image">
+          </div>
+          <h2 className="card__title">Биокотлета из марсианской Магнолии</h2>
+          <div className="card__info">
+            <div className="card__info_item">
+              <span className="card__info_name">Калории,ккал</span>
+              <span className="card__info_value">Калории,ккал</span>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
