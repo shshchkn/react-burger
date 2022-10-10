c313cc3440aeca9b01fca3cea6d20f04635a05f6
Alex White <shshchkn@yandex.ru>
2022-10-10 19:56:08 +0200

    Header rebuild

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -1,70 +1,32 @@
-import React from "react";
 import {Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components'
 
 import styles from './app-header.module.scss';
-
-type NavTypes = {
-  children: React.ReactNode;
-};
-
-const Nav = ({children}: NavTypes) => {
-  return (
-    <nav className={styles.header__nav}>
-      <ul className={`${styles.header__nav_menu} menu`} role="list">
-        {children}
-      </ul>
-    </nav>
-  );
-}
-
-type NavItemTypes = {
-  name: string,
-  icon: React.ReactNode,
-  active: boolean
-};
-
-const NavItem = ({name, icon, active}: NavItemTypes) => {
-  return (
-    <li className={styles.menu__item}>
-      <a href="#" className={`${styles.menu__item_link} text text_type_main-default text_color_inactive ${active ? styles.menu__item_link_active : ''}`}>
-        {icon}{name}
-      </a>
-    </li>
-  );
-}
-
-const HeaderLogo = () => {
-  return (
-    <div className={styles.header__logo}>
-      <a href="/" className={styles.header__logo_link} title="Stellar Burger"><Logo/></a>
-    </div>
-  );
-}
-
-type AccountLinkTypes = {
-  name: string,
-  icon: React.ReactNode,
-};
-
-const AccountLink = ({name, icon}: AccountLinkTypes) => {
-  return (
-    <a href="#" className={`${styles.header__account} text text_type_main-default text_color_inactive`}>
-      {icon}{name}
-    </a>
-  );
-}
 
 const AppHeader = () => {
   return (
     <header className={`${styles.header} pt-4 pb-4`}>
       <div className="container container--wide">
         <div className={styles.header__inner}>
-          <Nav>
-            <NavItem name="Конструктор" icon={<BurgerIcon type="secondary"/>} active={true} />
-            <NavItem name="Лента заказов" icon={<ListIcon type="secondary"/>} active={false} />
-          </Nav>
-          <HeaderLogo />
-          <AccountLink name="Личный кабинет" icon={<ProfileIcon type='secondary'/>}/>
+          <nav className={styles.header__nav}>
+            <ul className={`${styles.header__nav_menu} menu`} role="list">
+              <li className={styles.menu__item}>
+                <a href="#" className={`${styles.menu__item_link} text text_type_main-default text_color_inactive ${styles.menu__item_link_active}`}>
+                  <BurgerIcon type="secondary"/> Конструктор
+                </a>
+              </li>
+              <li className={styles.menu__item}>
+                <a href="#" className={`${styles.menu__item_link} text text_type_main-default text_color_inactive`}>
+                  <ListIcon type="secondary"/> Лента заказов
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className={styles.header__logo}>
+            <a href="/" className={styles.header__logo_link} title="Stellar Burger"><Logo/></a>
+          </div>
+          <a href="#" className={`${styles.header__account} text text_type_main-default text_color_inactive`}>
+            <ProfileIcon type='secondary'/> Личный кабинет
+          </a>
         </div>
       </div>
     </header>
