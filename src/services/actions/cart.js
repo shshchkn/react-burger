5f9cd0457f2f83d88bdfea494ad59116e26390bc
Alex White <shshchkn@yandex.ru>
2022-10-26 20:23:34 +0200

    Actions upd.

---
+++ src/services/actions/cart.js
@@ -0,0 +1,6 @@
+export const ADD_CART_BUN = 'ADD_CART_BUN';
+export const ADD_CART_ITEM = 'ADD_CART_ITEM';
+export const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
+
+export const UPDATE_CART = 'UPDATE_CART';
+export const CLEAN_CART = 'CLEAN_CART';
---
+++ src/services/actions/ingredients.js
@@ -0,0 +1,18 @@
+import {API_URL, apiRequest} from "../../utils/burger-api";
+
+export const GET_ITEMS_REQUEST = 'GET_ITEMS_REQUEST';
+export const GET_ITEMS_SUCCESS = 'GET_ITEMS_SUCCESS';
+export const GET_ITEMS_FAILED = 'GET_ITEMS_FAILED';
+
+export const getItems = () => dispatch => {
+  dispatch({type: GET_ITEMS_REQUEST});
+  apiRequest(`${API_URL}/ingredients`)
+    .then(res => {
+      if (res && res.success) {
+        dispatch({type: GET_ITEMS_SUCCESS, items: res.data});
+      } else {
+        dispatch({type: GET_ITEMS_FAILED});
+      }
+    })
+    .catch(err => console.log(err));
+}
