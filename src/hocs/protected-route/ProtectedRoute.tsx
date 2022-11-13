fb43e471092f820d10bb0cf7d3dbd7d4db4cc2e2
Alex White <shshchkn@yandex.ru>
2022-11-13 11:51:01 +0200

    Protected route upd.

--- src/hocs/protected-route/ProtectedRoute.tsx
+++ src/hocs/protected-route/ProtectedRoute.tsx
@@ -1,27 +1,21 @@
+import React from "react";
 import {Navigate, Outlet, useLocation} from "react-router-dom";
 import {getCookie} from "../../utils/helpers";
 
-const ProtectedRoute = () => {
-  const {pathname} = useLocation();
-  const token = getCookie('accessToken');
+const ProtectedRoute = ({anonymous = false }) => {
+  const isLoggedIn = getCookie('accessToken');
+  const location = useLocation();
+  const from = location.state?.from || '/';
 
-  if (
-    (token !== undefined && (
-      pathname === '/register' ||
-      pathname === '/forgot-password' ||
-      pathname === '/reset-password'
-    )) ||
-    (!token && (
-      pathname === '/profile' ||
-      pathname === '/profile/orders' ||
-      pathname === '/profile/orders/:id'
-    ))
-  ) {
-    return <Navigate to="/login" replace />
+  if (anonymous && isLoggedIn) {
+    return <Navigate to={ from } />;
+  }
+
+  if (!anonymous && !isLoggedIn) {
+    return <Navigate to="/login" state={{ from: location}}/>;
   }
 
   return <Outlet />;
-
 }
 
 export default ProtectedRoute;
